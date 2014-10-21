743fad2255142abc6c35d4ce405103ad43e6850d
LithiaH <lithiah@users.noreply.github.com>
2014-10-20 22:17:10 -0400

    Import connections to filter and display
    Successfully imports each connection from connections.json, and creates
    a div for it in connectionfilter.html
    
    Right now, toggle of entire list of connections visibility is triggered
    by changes in the form
    
    these divs still need to be fully integrated into original page

--- ourjavascript.js
+++ ourjavascript.js
@@ -9,21 +9,36 @@ $(document).ready(function() {
 		dataType: "json",
 		async: false,
 		success: function (data) {
-		alert('got data!');
+			
+			var connections = data["ConnectionDescriptions"];
+
+			var i = 1;
+
+			$.each(connections, function() {
+
+				var D = this["CourseDescription"];
+				var N = this["CourseName"];
+				var end = D.length - 43;
+
+				currentText = $("#jsonfill").html();
+
+				$("#jsonfill").html(currentText + "<div id='con" + i + "' <br>" + N + "<br>" + D.substring(0, end) + "</div>");
+				
+				i = i + 1;
+
+			});
+
+			$('.uk-form').change(function(){
+
+    				$('#jsonfill').toggle();
+  			});
+
 		},
 		error: function(xhr, status) {
 		alert("hatada:" + xhr.responseXML);
   		}
 	});
-
-
-
-
-  $('.connectiondescriptions').hide();
   
-  $('#selectionform').change(function(){
-    $('.connectiondescriptions').hide();
-  	$('.onesWEWANT').show();
-  });
+  
   
 });
