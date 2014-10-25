9c434e8c914b582b32d28442ca090881d8349b7c
LithiaH <lithiah@users.noreply.github.com>
2014-10-25 16:01:52 -0400

    work on class selectors for filter

--- ourjavascript.js
+++ ourjavascript.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 								
 				var newdiv = "#con" + i
 
-				$(newdiv).addClass("two");
+				$(newdiv).addClass("connection");
 
 				if (Three == "True") {
 					$(newdiv).addClass("three");
@@ -50,6 +50,8 @@ $(document).ready(function() {
 					$(newdiv).addClass("qa");
 				};
 
+				$(newdiv).hide();
+
 				i = i + 1;
 			});
 
@@ -57,60 +59,43 @@ $(document).ready(function() {
 		error: function(xhr, status) {
 		alert("hatada:" + xhr.responseXML);
   		}
-	});
-  	
-  	$(function(){ 
-  		$('.2or3').find(':checkbox').each(function(){
-
-  		console.log(this);
-        $(this).attr('checked', true);
-
-    	});
-    });   
+	}); 
 
 	$('.uk-form').change(function(){
 
-		// 2 or 3 courses
-		if ($('#2course').is(":checked")) {
-			$('.two').show();
-		}
-		else {
-			$('.two').hide();
-		}
-		if ($('#3course').is(":checked")) {
-			$('.three').show();
-		}
-		else {
-			$('.three').hide();
-		}
-		
-		// division
-		if ($('#natsci').is(":checked")) {
-			$('.natural').show();
-		}
-		else {
-			$('.natural').hide();
-		}
-		if ($('#socsci').is(":checked")) {
-			$('.social').show();
-		}
-		else {
-			$('.social').hide();
-		}
-		if ($('#hum').is(":checked")) {
-			$('.humanities').show();
-		}
-		else {
-			$('.humanities').hide();
-		}	
-		if ($('#qa').is(":checked")) {
-			$('.qa').show();
-		}
-		else {
-			$('.qa').hide();
-		}
+		$('.connection').each(function() {
+			var two = false;
+			var three = false;
+			var coursecount = false;
+			var division = false;
 
-    				
+			if ($('#2course').is(":checked")) {
+				two = true;
+			}
+			if ($('#3course').is(":checked")) {
+				three = true;
+			}
+
+			if ($(this).hasClass('three') && three) {
+				coursecount = true;
+				two = false;
+			} 
+
+			if ( ($('#natsci').is(":checked") && $(this).hasClass('natural')) 
+				|| ($('#socsci').is(":checked") && $(this).hasClass('social')) 
+				|| ($('#hum').is(":checked") && $(this).hasClass('humanities')) 
+				|| ($('#qa').is(":checked") && $(this).hasClass('qa')) ) {
+				division = true;
+			}
+			
+			if ((coursecount || two) && division) {
+				$(this).show();
+			}
+			else {
+				$(this).hide();
+			}
+		});
+
   	});
   
 });
